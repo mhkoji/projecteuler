eda795bd7ac9780f72f407f64dc9021faf2466a5
mhkoji <mhkoji2+git@gmail.com>
2022-08-22 21:02:12 +0900

    Update p1

--- p1.lisp
+++ p1.lisp
@@ -1,4 +1,6 @@
-(loop for i from 1 below 1000
-   when (or (= (mod i 3) 0)
-            (= (mod i 5) 0))
-     sum i)
+;233168
+(defun main ()
+  (loop for i from 1 below 1000
+        when (or (= (mod i 3) 0)
+                 (= (mod i 5) 0))
+        sum i))
